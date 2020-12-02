ea9caab8185c43c723a22ad7f5b0efb3a6640905
HusamAjour <husamaj278@gmail.com>
2020-12-02 20:50:25 +0200

    revert wrong push

--- data/migrations/1606914444357-connect-books-and-authors-tables.sql
+++
@@ -1,3 +0,0 @@
-/*Timestamp: 1606914444357 */
-/* set the author_id as the foreign key of the books table */
-ALTER TABLE books ADD CONSTRAINT fk_authors FOREIGN KEY (author_id) REFERENCES authors(id);
--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,13 @@
 $('#hamburgerIcon').on('click', ()=>{
   $('#linksBlock').slideToggle();
 });
+
+$('#updateForm').hide();
+
+$('#updateBtn').on('click', ()=>{
+  $('#updateForm').slideToggle();
+});
+
+$('#closeBtn').on('click', ()=>{
+  $('#updateForm').slideToggle();
+});
